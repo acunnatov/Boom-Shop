2befb966283efb7c7964dc1b2fcee71588b3cfbc
Abdulbosit <acunnatov@gmail.com>
2024-06-12 10:46:10 +0500

    save

--- build.js
+++ build.js
@@ -1,14 +1,25 @@
-import fs from 'fs'
-const Handlebars = require('handlebars');
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
+import { dirname } from 'path';
+import Handlebars from 'handlebars';
 
 const compileTemplate = (templatePath, dataPath, outputPath) => {
-  const template = fs.readFileSync(templatePath, 'utf8');
+  const template = readFileSync(templatePath, 'utf8');
   const templateScript = Handlebars.compile(template);
-  const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  const data = JSON.parse(readFileSync(dataPath, 'utf8'));
   const html = templateScript(data);
 
-  fs.writeFileSync(outputPath, html);
+  // Ensure the output directory exists
+  const outputDir = dirname(outputPath);
+  if (!existsSync(outputDir)) {
+    mkdirSync(outputDir, { recursive: true });
+  }
+
+  writeFileSync(outputPath, html);
 };
 
 // Example usage
-compileTemplate('views/index.hbs', 'data/index.json', 'dist/index.html');
+const templatePath = new URL('./views/index.hbs', import.meta.url).pathname;
+const dataPath = new URL('./data/index.json', import.meta.url).pathname;
+const outputPath = new URL('./dist/index.html', import.meta.url).pathname;
+
+compileTemplate(templatePath, dataPath, outputPath);
