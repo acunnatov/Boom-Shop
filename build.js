60c87d0846ebc7b496b8ae5d7d31aab03a601f4c
Abdulbosit <acunnatov@gmail.com>
2024-06-12 10:48:37 +0500

    save

--- build.js
+++ build.js
@@ -2,6 +2,11 @@ import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 import { dirname } from 'path';
 import Handlebars from 'handlebars';
 
+// Full paths to your files
+const templatePath = 'C:/Users/user/Desktop/JavaScript/app/views/index.hbs';
+const dataPath = 'C:/Users/user/Desktop/JavaScript/app/data/index.json';
+const outputPath = 'C:/Users/user/Desktop/JavaScript/app/dist/index.html';
+
 const compileTemplate = (templatePath, dataPath, outputPath) => {
   const template = readFileSync(templatePath, 'utf8');
   const templateScript = Handlebars.compile(template);
@@ -18,8 +23,4 @@ const compileTemplate = (templatePath, dataPath, outputPath) => {
 };
 
 // Example usage
-const templatePath = new URL('./views/index.hbs', import.meta.url).pathname;
-const dataPath = new URL('./data/index.json', import.meta.url).pathname;
-const outputPath = new URL('./dist/index.html', import.meta.url).pathname;
-
 compileTemplate(templatePath, dataPath, outputPath);
